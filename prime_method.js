0103713af3dc8e3b6bd4426a8831c0a1d845f182
Ranjeet <ranjeetraj44666@gmail.com>
2023-01-16 16:59:08 +0530

    Prime problem is done

---
+++ prime_method.js
@@ -0,0 +1,16 @@
+Prime Mthod.
+
+let num=13;
+let count=0;
+for(let i=1;i<=num;i--){
+if(num%i==0){
+count++;
+}
+}
+
+if(count==2){
+console.log("it is prime");
+}
+else{
+console.log("it is not a prime")
+}
